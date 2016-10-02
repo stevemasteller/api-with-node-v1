d5751adf363e4217781383ae8fbef7db1042f0e5
stevemasteller <stevermasteller@gmail.com>
2016-10-02 09:35:24 -0400

    Cleaned up code. didn't change functionality
    error handling doesn't work

--- src/app.js
+++ src/app.js
@@ -4,36 +4,9 @@ var express = require('express');
 
 var app = express();
 
-//set view engine to serve middleware
-app.set('view engine', 'pug');
-//set where to look for templates
-app.set('views', __dirname + '/templates');
-
-// set folder structure for middleware to be able to call stylesheets
-app.use('/static', express.static(__dirname + '/public'));
-
-app.use('/', indexPage);
-
-var router = express.Router();
-
-/* GET index page. */
-router.get('/', function(req, res, next) {
-  indexPage();
-});
-
-// set up server on Port 3000
-app.listen(3000, function() {
-	console.log("The frontend server is running on port 3000!");
-});
-
-
-
-
-//Twitter App
-
 var Twitter = require('twitter');
 
-//Get this data from your twitter apps dashboard and put it in ./src/twitter-api-secrets.json
+//Get this data from your twitter apps dashboard and put it in ./src/config.json
 //    {
 //        "consumer_key": "XXX",
 //        "consumer_secret": "XXX",
@@ -46,36 +19,44 @@ var config = require( __dirname + '/config.json');
 
 var twitter = new Twitter(config);
 
+//set view engine to serve middleware
+app.set('view engine', 'pug');
+//set where to look for templates
+app.set('views', __dirname + '/templates');
 
-function indexPage() {
-	twitter.get('users/show',{screen_name: config.screen_nam}, function (error, userObj, response) {
-		if (error) {
-			errMasg;
-		} else {
+// set folder structure for middleware to be able to call stylesheets
+app.use('/static', express.static(__dirname + '/public'));
+
+// set up server on Port 3000
+app.listen(3000, function() {
+	console.log("The frontend server is running on port 3000!");
+});
+
+//Error handler
+app.on('uncaughtException', function (err) {
+    console.log(err);
+	res.render('error', err);
+}); 
+
+//Twitter App
+app.get('/', function(req, res) {
+	twitter.get('users/show',{screen_name: config.screen_name}, function (err, userObj, response) {
+		if (!err) {
 			
-			twitter.get('statuses/user_timeline',{screen_name: config.screen_name, count: '5'}, function (error, tweetsObj, response) {
-				if (error) {
-					errMsg;
-				} else {
+			twitter.get('statuses/user_timeline',{screen_name: config.screen_name, count: '5'}, function (err, tweetsObj, response) {
+				if (!err) {
 					
-					twitter.get('/friends/list.json',{screen_name: config.screen_name, count: '5'}, function (error, friendsObj, response) {
-						if (error) {
-							errMsg;
-						} else {
+					twitter.get('/friends/list.json',{screen_name: config.screen_name, count: '5'}, function (err, friendsObj, response) {
+						if (!err) {
 
-							twitter.get('direct_messages/sent.json',{screen_name: config.screen_name, count: '5'}, function (error, messagesObj, response) {
-								if (error) {
-									errMsg;
-								} else {
+							twitter.get('direct_messages/sent.json',{screen_name: config.screen_name, count: '5'}, function (err, messagesObj, response) {
+								if (!err) {
 								
-									app.get('/', function (req, res) {
-										
-										res.render('index', {
-											userObj: userObj,
-											tweetsObj: tweetsObj,
-											friendsObj: friendsObj.users,
-											messagesObj: messagesObj
-										});
+									res.render('index', {
+										userObj: userObj,
+										tweetsObj: tweetsObj,
+										friendsObj: friendsObj.users,
+										messagesObj: messagesObj
 									});
 								}
 							});
@@ -85,4 +66,4 @@ function indexPage() {
 			});
 		}
 	});
-}
+});
